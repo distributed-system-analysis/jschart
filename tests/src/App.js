9621962eb381dd306089a36ec21a89f4c7f85ded
Gurbir Kalsi <kalsigurbir@gmail.com>
2019-02-06 11:18:38 -0500

    Revert "Add param to load and parse JSON objects dynamically"

--- tests/src/App.js
+++ tests/src/App.js
@@ -1,24 +1,15 @@
-import React, { Component } from "react";
-import jschart from "jschart";
+import React, { Component } from 'react';
+import axios from 'axios';
+import jschart from 'jschart';
 
 class App extends Component {
+
   componentDidMount = () => {
-    jschart.create_jschart(
-      0,
-      "timeseries",
-      "jschart_timeseries",
-      "Timeseries Demo",
-      "Time (secs.)",
-      "CPU Utilization",
-      {
-        json_object: {
-          x_axis_series: "time",
-          data_series_names: ["time", "data series 1", "data series 2"],
-          data: [[1461027782000, 0, 1], [1461027783000, 4, 5]]
-        }
-      }
-    );
-  };
+    //Reference a sample CSV file from the production environment. 
+    axios.get("{{ production_url }}").then(res => {
+      jschart.create_jschart(0, "histogram", "jschart_histogram", "Histogram Demo", "Buckets", "Bucket Size", { csvfiles: [ res.data ] })    
+    });
+  }
 
   render() {
     return (
@@ -27,7 +18,7 @@ class App extends Component {
           <h1 className="App-title">JSChart Node Module Demo</h1>
         </header>
         <br></br>
-        <div id="jschart_timeseries"></div>
+        <div id="jschart_histogram"></div>
       </div>
     );
   }
