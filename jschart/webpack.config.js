287473ec0876a432d83943e77764ea695a033b2c
Karl Rister <krister@redhat.com>
2019-05-08 15:41:51 +0000

    update demo app to include additional examples
    - Use file-loader module to access data files

--- jschart/webpack.config.js
+++ jschart/webpack.config.js
@@ -1,16 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'index.js',
+    entry: 'index.js',
     module: {
-    rules: [
-        {
-            test: /\.css$/,
-            use: [
-            'style-loader',
-            'css-loader'
-            ]
-        }
+	rules: [
+            {
+		test: /\.css$/,
+		use: [
+		    'style-loader',
+		    'css-loader'
+		]
+            },
+	    {
+		test: /\.(csv|json|plot)$/,
+		use: [
+		    loader: 'file-loader',
+		    options: {}
+		]
+	    }
         ]
     }
 };
--- tests/src/App.js
+++ tests/src/App.js
@@ -1,34 +1,102 @@
 import React, { Component } from "react";
 import jschart from "jschart";
 
+import histogram_data from './data/histogram.csv';
+import timeseries_data from './data/timeseries.csv';
+import xy_data from './data/xy.csv';
+import jitter_data from './data/jitter.csv';
+
 class App extends Component {
   componentDidMount = () => {
     jschart.create_jschart(
       0,
       "timeseries",
-      "jschart_timeseries",
-      "Timeseries Demo",
+      "jschart_json",
+      "JSON Demo",
       "Time (secs.)",
       "CPU Utilization",
       {
-        json_object: {
-          x_axis_series: "time",
-          data_series_names: ["time", "data series 1", "data series 2"],
-          data: [[1461027782000, 0, 1], [1461027783000, 4, 5]]
-        }
+          json_object: {
+	      x_axis_series: "time",
+	      data_series_names: ["time", "data series 1", "data series 2"],
+	      data: [[1461027782000, 0, 1], [1461027783000, 4, 5]]
+	  }
       }
     );
+    jschart.create_jschart(
+      0,
+      "histogram",
+      "jschart_histogram",
+      "Histogram Demo",
+      "Buckets",
+      "Bucket Size",
+      {
+	  csvfiles: [ histogram_data ]
+      }
+    );
+    jschart.create_jschart(
+      1,
+      "timeseries",
+      "jschart_timeseries",
+      "Timeseries Demo",
+      null,
+      null,
+      {
+	  csvfiles: [ timeseries_data ],
+	  sort_datasets: false
+      }
+    );
+    jschart.create_jschart(
+      0,
+      "xy",
+      "jschart_xy",
+      "XY Demo",
+      null,
+      null,
+      {
+	  csvfiles: [ xy_data ]
+      }
+    );
+    jschart.create_jschart(
+      0,
+      "xy",
+      "jschart_jitter",
+      "Jitter Samples",
+      "Samples",
+      "Latency (ns)",
+      {
+	  csvfiles: [ jitter_data ]
+      }
+    );
+    jschart.create_jschart(
+      0,
+      "xy",
+      "jschart_jitter_scatter",
+      "Jitter Samples: Scatterplot",
+      "Samples",
+      "Latency (ns)",
+      {
+	  csvfiles: [ jitter_data ],
+	  scatterplot: true
+      }
+    );
+    jschart.finish_page();
   };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">JSChart Node Module Demo</h1>
+          <h1 className="App-title">JSChart Demos</h1>
         </header>
         <br></br>
+        <div id="jschart_json"></div>
+        <div id="jschart_histogram"></div>
         <div id="jschart_timeseries"></div>
-      </div>
+        <div id="jschart_xy"></div>
+        <div id="jschart_jitter"></div>
+        <div id="jschart_jitter_scatter"></div>
+	</div>
     );
   }
 }
