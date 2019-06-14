50f94102658e58878b521415ac9c96c54d95815b
Karl Rister <krister@redhat.com>
2019-06-14 04:05:59 +0000

    cdm-tests: add mpstat chart

--- cdm-tests/src/App.js
+++ cdm-tests/src/App.js
@@ -18,6 +18,13 @@ import iostat_cdm_5 from './data/iostat-iops-host-perf36-dev-sda-type.json';
 import iostat_cdm_6 from './data/iostat-iops-host-perf84-dev-nvme0n1-type.json';
 import iostat_cdm_7 from './data/iostat-iops-host-dev-type.json';
 
+import mpstat_cdm_1 from './data/mpstat-busy.json';
+import mpstat_cdm_2 from './data/mpstat-busy-host.json';
+import mpstat_cdm_3 from './data/mpstat-busy-host-socket.json';
+import mpstat_cdm_4 from './data/mpstat-busy-host-socket-core.json';
+import mpstat_cdm_5 from './data/mpstat-busy-host-socket-core-id.json';
+import mpstat_cdm_6 from './data/mpstat-busy-host-socket-core-id-mode.json';
+
 var fio_choices = [
   {
     "label": "Null",
@@ -134,10 +141,65 @@ function setup_iostat_chart_selection_box() {
     .text(function(d) { return d.label; });
 }
 
+var mpstat_choices = [
+  {
+    "label": "Null",
+    "object": { }
+  },
+  {
+    "label": "Busy",
+    "object": { cdmjson_object: mpstat_cdm_1 }
+  },
+  {
+    "label": "Busy - Per Host",
+    "object": { cdmjson_object: mpstat_cdm_2 }
+  },
+  {
+    "label": "Busy - Per Host, Per Socket",
+    "object": { cdmjson_object: mpstat_cdm_3 }
+  },
+  {
+    "label": "Busy - Per Host, Per Socket, Per Core",
+    "object": { cdmjson_object: mpstat_cdm_4 }
+  },
+  {
+    "label": "Busy - Per Host, Per Socket, Per Core, Per ID",
+    "object": { cdmjson_object: mpstat_cdm_5 }
+  },
+  {
+    "label": "Busy - Per Host, Per Socket, Per Core, Per ID, Per Mode",
+    "object": { cdmjson_object: mpstat_cdm_6 }
+  }
+];
+for (i = 0; i < mpstat_choices.length; i++) {
+  mpstat_choices[i].index = i;
+}
+
+function change_mpstat_selection() {
+  jschart.chart_set_title("jschart_cdm_mpstat", mpstat_choices[this.value].label);
+  jschart.chart_set_y_axis_label("jschart_cdm_mpstat", "Y Axis");
+  jschart.chart_set_x_axis_label("jschart_cdm_mpstat", "X Axis");
+  jschart.chart_reload_options("jschart_cdm_mpstat", mpstat_choices[this.value].object);
+}
+
+function setup_mpstat_chart_selection_box() {
+  var myselect = d3.select("#select_mpstat_dataset")
+    .append("select")
+    .on("change", change_mpstat_selection)
+
+  myselect.selectAll(".options")
+    .data(mpstat_choices)
+    .enter()
+    .append("option")
+    .attr("value", function(d) { return d.index; })
+    .text(function(d) { return d.label; });
+}
+
 class App extends Component {
   componentDidMount = () => {
     setup_fio_chart_selection_box();
     setup_iostat_chart_selection_box();
+    setup_mpstat_chart_selection_box();
 
     jschart.create_jschart(
       1,
@@ -155,6 +217,15 @@ class App extends Component {
       "IOSTAT",
       "Time",
       "IOSTAT",
+      {}
+    );
+    jschart.create_jschart(
+      1,
+      "timeseries",
+      "jschart_cdm_mpstat",
+      "MPSTAT",
+      "Time",
+      "MPSTAT",
       {}
     );
     jschart.finish_page();
@@ -175,6 +246,11 @@ class App extends Component {
         <div id="jschart_cdm_iostat">
           <h3 id="select_iostat_dataset">
 	    IOSTAT Chart Dataset Selection:
+	  </h3>
+        </div>
+        <div id="jschart_cdm_mpstat">
+          <h3 id="select_mpstat_dataset">
+	    MPSTAT Chart Dataset Selection:
 	  </h3>
         </div>
       </div>
